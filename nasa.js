da6a56d05f7fe8f76a49d5b38483e68704a57064
Sarah Spikes <sarah@effectivealtruism.org>
2018-05-04 10:35:39 -0700

    basic ajax examples

---
+++ nasa.js
@@ -0,0 +1,33 @@
+
+const request = require('request');
+
+// callback based
+const baseUrl = 'https://swapi.co/api/'
+request(baseUrl + 'planets/1', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  // console.log(res)
+  console.log(body);
+});
+
+
+
+const axios = require('axios');
+
+// promise-based
+axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+.then(response => {
+  console.log(response.data.url);
+  console.log(response.data.explanation);
+})
+.catch(error => {
+  console.log(error);
+});
+
+// jquery
+$.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+.then(response => {
+  console.log(response)
+})
+.catch(error => {
+  console.log(error);
+});
